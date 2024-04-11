da8749c50f321b4d47828dc5408b7e3daa07374b
taemin <xoals6536@naver.com>
2024-04-11 17:24:17 +0900

    WebMvcConfig 수정

--- modules/api/user-api/src/main/java/com/daehan/frozen/userapi/config/WebMvcConfig.java
+++ modules/api/user-api/src/main/java/com/daehan/frozen/userapi/config/WebMvcConfig.java
@@ -7,13 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**") // CORS를 적용할 엔드포인트 경로
+        registry.addMapping("/**") // CORS를 적용할 엔드포인트 경로
                 .allowedOrigins("*") // 모든 도메인에서의 요청 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용된 HTTP 메소드
                 .allowedHeaders("*"); // 요청 헤더에서 허용할 내용
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
     }
 }
