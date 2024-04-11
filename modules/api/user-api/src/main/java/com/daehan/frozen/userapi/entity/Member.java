4ea65ed03cf8c0ecf0aed90bf89b52101a60bdcf
taemin6536 <xoals6536@naver.com>
2024-04-11 23:29:34 +0900

    update Member

--- modules/api/user-api/src/main/java/com/daehan/frozen/userapi/entity/Member.java
+++ modules/api/user-api/src/main/java/com/daehan/frozen/userapi/entity/Member.java
@@ -2,11 +2,14 @@ package com.daehan.frozen.userapi.entity;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Member {
     private String id;
     private String name;
     private String passwd;
     private String email;
-
+    private boolean roles; //0 : 일반유저, 1 : 관리자
+    private Date regDate;
 }
