86241081353cfafb608e3aa07cc92431c872ba76
taemin <xoals6536@naver.com>
2024-05-14 17:18:11 +0900

    Update jwtAuthenticationFilter doFilter

--- modules/api/user-api/src/main/java/com/daehan/frozen/userapi/entity/Member.java
+++ modules/api/user-api/src/main/java/com/daehan/frozen/userapi/entity/Member.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 import java.util.Date;
-import java.util.List;
 
 
 @Data
--- modules/api/user-api/src/main/java/com/daehan/frozen/userapi/jwt/JwtAuthenticationFilter.java
+++ modules/api/user-api/src/main/java/com/daehan/frozen/userapi/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.daehan.frozen.userapi.jwt;
 
-import com.daehan.frozen.userapi.entity.Member;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,8 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,7 +28,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = parseBearerToken(request);
+        User user = parseUserSpecification(token);
+        AbstractAuthenticationToken authenticated = UsernamePasswordAuthenticationToken.authenticated(user, token, user.getAuthorities());
+        authenticated.setDetails(new WebAuthenticationDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticated);
 
+        filterChain.doFilter(request, response);
     }
 
     private String parseBearerToken(HttpServletRequest request) {
